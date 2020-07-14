38a6659c385a9a84aed1f1174e13d971469627f6
unknown <hasanmaroof93@gmail.com>
2020-07-14 22:08:18 +0100

    Updates

---
+++ precache-manifest.ad23137fc8512bbbe4b22cbaa9a997e0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79fbef803f0ec0c4be47948ebdb8e12c",
+    "url": "/MyPortfoli-v2/index.html"
+  },
+  {
+    "revision": "59652923f41c076fd60b",
+    "url": "/MyPortfoli-v2/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "294318a213322aa8e3b5",
+    "url": "/MyPortfoli-v2/static/js/2.ec5a3acf.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/MyPortfoli-v2/static/js/2.ec5a3acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59652923f41c076fd60b",
+    "url": "/MyPortfoli-v2/static/js/main.40e03958.chunk.js"
+  },
+  {
+    "revision": "c81aa5137a8ca8e1693d",
+    "url": "/MyPortfoli-v2/static/js/runtime-main.cf0428ea.js"
+  },
+  {
+    "revision": "da7da7fb8f73f013e06f54e503a43130",
+    "url": "/MyPortfoli-v2/static/media/Hasan_UI_Specialist.da7da7fb.pdf"
+  },
+  {
+    "revision": "a2372ee74b13a20bf7f1ce6aafb367fa",
+    "url": "/MyPortfoli-v2/static/media/Intro.a2372ee7.jpeg"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/MyPortfoli-v2/static/media/Redux.c8f44df3.png"
+  },
+  {
+    "revision": "265464f362d392c824eee0eefd608b4c",
+    "url": "/MyPortfoli-v2/static/media/agile.265464f3.jpg"
+  },
+  {
+    "revision": "68fcf38c8c8e178799686d15abef5944",
+    "url": "/MyPortfoli-v2/static/media/bootstrap.68fcf38c.png"
+  },
+  {
+    "revision": "0bcb32c6d86cf5aa6cd6bc1435f8665f",
+    "url": "/MyPortfoli-v2/static/media/git.0bcb32c6.png"
+  },
+  {
+    "revision": "f4b98c10645b596b970c5602e67e54b7",
+    "url": "/MyPortfoli-v2/static/media/hasan_sidebar.f4b98c10.jpg"
+  },
+  {
+    "revision": "9edcb5106c553737a172ec833810fe2e",
+    "url": "/MyPortfoli-v2/static/media/jest.9edcb510.png"
+  },
+  {
+    "revision": "7e531ab08821d8d1f28693249b526a35",
+    "url": "/MyPortfoli-v2/static/media/json.7e531ab0.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/MyPortfoli-v2/static/media/mui.d2b1e190.png"
+  },
+  {
+    "revision": "5b47d4f8d197911857e8e77cb4dd1569",
+    "url": "/MyPortfoli-v2/static/media/nodejs-logo.5b47d4f8.png"
+  },
+  {
+    "revision": "4e96f1110cb872407e837d4fc1440f96",
+    "url": "/MyPortfoli-v2/static/media/ts.4e96f111.png"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/MyPortfoli-v2/static/media/vsCode.225fbbe0.png"
+  },
+  {
+    "revision": "c500a3801d8356a86da86a06c3d13a4d",
+    "url": "/MyPortfoli-v2/static/media/webpack-logo.c500a380.png"
+  }
+]);
